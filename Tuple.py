b4099f41bce7fdf9c061f14914dfa085c371e18c
Shreyaa0001 <jadhavshreyaa3@gmail.com>
2026-01-27 18:39:02 +0530

    Update Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,13 @@
+# 3.1 Create and access
+my_tuple = (10, 20, 30)
+print("Tuple:", my_tuple)
+print("Access element at index 1:", my_tuple[1])
+
+# 3.2 Nested Tuple
+nested_tuple = (1, 2, (3, 4), 5)
+print("Nested Tuple:", nested_tuple)
+print("Access nested element:", nested_tuple[2][1])
+
+# 3.3 Repetition of tuple
+repeated = my_tuple * 2
+print("Repeated Tuple:", repeated)
